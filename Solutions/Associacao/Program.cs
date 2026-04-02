82c69f88fe5d571f125846bb3e910c252a68afab
EduardoGabri356 <eduardo1227edu@gmail.com>
2026-04-02 14:13:50 -0300

    Adicionando Trabalho e Aula da semana

---
+++ Solutions/Associacao/Program.cs
@@ -0,0 +1,59 @@
+﻿Fornecedor forn1 = new Fornecedor("Empresa1", "11111111");
+Produto prod1 = new Produto("Produto1", 10.20);
+Categoria cat1 = new Categoria("Categoria1");
+
+// associação bilateral produto -> fornecedor
+prod1.Fornecedores.Add(forn1);
+
+// agregação unilateral produto -> categoria
+prod1.CategoriaProduto.Add(cat1);
+
+
+// associação bilateral fornecedor -> produto
+Fornecedor forn2 = new Fornecedor("Empresa2", "22222222");
+Produto prod2 = new Produto("Produto2", 15.50);
+Produto prod3 = new Produto("Produto3", 100);
+
+forn2.Produtos.Add(prod2);
+forn2.Produtos.Add(prod3);
+
+Console.WriteLine($"Nome:{prod1.Nome}/nPreço:{prod1.Preco}/nCategoria:{prod1.CategoriaProduto.Descritivo}");
+{
+    Console.WriteLine($"Razão Social:{forn2.RazaoSocial} - CNPJ: {forn2.cnpj}");
+}
+
+Console.WriteLine($"Outro Lado Bilateral");
+
+public class Produto
+{
+    public Produto(string nome, double preco)
+    { 
+        Nome = nome;
+        Preco = preco; 
+    }
+    public string? Nome { get; set; };
+    public double Preco { get; set; };
+    public Categoria CategoriaProduto {  get; set; }
+    public List<Fornecedor> Fornecedores = new List<Fornecedor>();
+}
+
+public class Categoria
+{
+    public Categoria(string descritivo)
+    {
+        Descritivo = descritivo;
+    }
+    public string Descritivo { get; set; }
+}
+
+public class Fornecedor
+{
+    public Fornecedor(string razaoSocial, string cnpj)
+    {
+        RazaoSocial = razaoSocial;
+        Cnpj = cnpj;
+    }
+    public string? RazaoSocial { get; set; }
+    public string? Cnpj { get; set; }
+    public List<Produto> Produtos = new List<Produto>();
+}
---
+++ Trabalho-oo-Vania/Trabalho-oo-Vania/Maquina.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Maquina : Equipamento
+{
+    public string? Modelo { get; set; }
+    public string? HoraOperacao { get; set; }
+    public Guid NumeroSerie { get; } = Guid.NewGuid();
+
+    private string? _observacao;
+    public string Observacao { set { _observacao = value; } }
+}
